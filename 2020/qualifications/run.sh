b86ffbcffa330b018a90ecc9e165664130202ca7
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-02-20 20:55:50 +0200

    Add script for running code

---
+++ 2020/qualifications/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PYTHON=python3
+
+$PYTHON solution.py a_example.txt a.out
+$PYTHON solution.py b_read_on.txt b.out
+$PYTHON solution.py c_incunabula.txt c.out
+$PYTHON solution.py d_tough_choices.txt d.out
+$PYTHON solution.py e_so_many_books.txt e.out
+$PYTHON solution.py f_libraries_of_the_world.txt f.out
