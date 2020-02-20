163bd3369213dcd789ed630f6a1daaacd79a1c3d
Stoian-Mihai <mihaistoianro@gmail.com>
2020-02-20 22:32:49 +0200

    Update solution.py

--- 2020/qualifications/solution.py
+++ 2020/qualifications/solution.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import argparse
 from collections import namedtuple
 import sys
@@ -9,8 +10,26 @@ parser.add_argument("output_file")
 args = parser.parse_args()
 
 
-Library = namedtuple("Library", ("books", "signup_time", "books_per_day"))
+class lib:
+    books = []
+    singup_time = 0
+    books_per_day = 0
+    id = 0
 
+    def __init__(self, books, singup_time, books_per_day, id):
+        self.books = books
+        self.singup_time = singup_time
+        self.books_per_day = books_per_day
+        self.id = id
+
+    def __repr__(self):
+        return '( books: ' + str(self.books) + ' singup_time: ' + str(self.singup_time) + ' books_per_day: ' + str(self.books_per_day) + ' )'
+
+    def __str__(self):
+        return '( books: ' + str(self.books) + ' singup_time: ' + str(self.singup_time) + ' books_per_day: ' + str(self.books_per_day) + ' )'
+
+
+next_id = 0
 with open(args.input_file) as fin:
     # num_books - number of unique books
     # num_libraries - number of unique libraries
@@ -21,16 +40,19 @@ with open(args.input_file) as fin:
 
     scores = [int(x) for x in next(fin).split()]
 
-    libraries = [None] * num_libraries
+    libraries = []
     for i in range(num_libraries):
         n, signup_time, books_per_day = map(int, next(fin).split())
 
         books = [int(x) for x in next(fin).split()]
+        libraries.append((lib(books, signup_time, books_per_day, next_id)))
+        next_id += 1
 
-        libraries[i] = Library(books, signup_time, books_per_day)
+
+libraries.sort(key=lambda x: x.books_per_day)
 
 with open(args.output_file, "w") as fout:
     print(num_libraries, file=fout)
     for idx, lib in enumerate(libraries):
-        print(idx, len(lib.books), file=fout)
+        print(lib.id, len(lib.books), file=fout)
         print(*lib.books, file=fout)
